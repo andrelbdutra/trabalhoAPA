29dd2fd03d5bbed4ddbddab416cdb6ef81236245
ferrarirod <ferrari_rod@hotmail.com>
2023-12-17 08:35:37 -0300

    projeto plotando gráficos em HTML

--- index.js
+++ index.js
@@ -1,184 +1,301 @@
 const fs = require('fs');
 
-// Função para criar um array de números de 1 a n embaralhados
-function criarArrayEmbaralhado(tamanho, grauDesordenamento) {
-  const array = Array.from({ length: tamanho }, (_, index) => index + 1);
+var pivotCase;
 
-  // Determinar o número de swaps com base no grau de desordem especificado
-  let numSwaps;
-  switch (grauDesordenamento) {
-    case 'pouco':
-      numSwaps = Math.floor(tamanho * 0.05);
-      break;
-    case 'medio':
-      numSwaps = Math.floor(tamanho * 0.25);
-      break;
-    case 'muito':
-      numSwaps = Math.floor(tamanho * 0.45);
-      break;
-    default:
-      numSwaps = 0;
-  }
-
-  // Realizar swaps aleatórios para criar o grau de desordem desejado
-  for (let i = 0; i < numSwaps; i++) {
-    const idx1 = Math.floor(Math.random() * tamanho);
-    const idx2 = Math.floor(Math.random() * tamanho);
-    [array[idx1], array[idx2]] = [array[idx2], array[idx1]];
-  }
-
-  return array;
+function getRandomInt(max) 
+{
+    return Math.floor(Math.random() * max);
 }
 
-// Função para trocar elementos de posição no array
-function trocar(array, i, j) {
-  const temp = array[i];
-  array[i] = array[j];
-  array[j] = temp;
+function getRandomArbitrary(min, max) 
+{
+    return Math.random() * (max - min + 1) + min;
 }
 
-// Função de particionamento para o quicksort
-function particionar(array, inicio, fim, pivo) {
-  while (inicio <= fim) {
-    while (array[inicio] < pivo) {
-      inicio++;
-    }
-    while (array[fim] > pivo) {
-      fim--;
-    }
-    if (inicio <= fim) {
-      trocar(array, inicio, fim);
-      inicio++;
-      fim--;
-    }
-  }
-  return inicio;
-}
-
-// Função de ordenação quicksort
-function quicksort(array, inicio, fim, tipoPivo) {
-  if (inicio < fim) {
-    let pivo;
-    switch (tipoPivo) {
-      case 'primeira':
-        pivo = array[inicio];
-        break;
-      case 'central':
-        pivo = array[Math.floor((inicio + fim) / 2)];
-        break;
-      case 'media':
-        pivo = pivoMedia(array.slice(inicio, fim));
-        break;
-      case 'aleatorio':
-        pivo = array[Math.floor(Math.random() * (fim - inicio)) + inicio];
-        break;
-      case 'mediana':
-        pivo = pivoMediana(array.slice(inicio, fim));
-        break;
-      case 'achaPivo':
-        pivo = achaPivo(array.slice(inicio, fim));
-        break;
-      default:
-        console.log('Pivo invalido!');
-        return;
+function averageArray(arr)
+{
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++)
+    {
+        sum += arr[i];
     }
 
-    const indiceParticao = particionar(array, inicio, fim, pivo);
-
-    quicksort(array, inicio, indiceParticao - 1, tipoPivo);
-    quicksort(array, indiceParticao, fim, tipoPivo);
-  }
-  return array;
+    return sum/arr.length;
 }
 
-// Função para encontrar o pivo média considerando o primeiro, central e último valores da lista
-function pivoMedia(array) {
-  const inicio = 0;
-  const fim = array.length - 1;
-  const first = array[inicio];
-  const middle = array[Math.floor((fim - inicio) / 2)];
-  const last = array[fim];
-  return Math.floor((first + middle + last) / 3);
-}
+function generateUnorderedArrayByDegree(deg,size)
+{
+    let arr = Array.from({length: size}, (_, i) => i + 1);
+    
+    let swaps = Math.floor(size * (deg/100));
 
-// Função para encontrar o pivo mediana
-function pivoMediana(array) {
-  const inicio = 0;
-  const meio = Math.floor((array.length - 1) / 2);
-  const fim = array.length - 1;
+    for(let i = 0; i < swaps; i++)
+    {
+        let i = getRandomInt(size);
+        let j = getRandomInt(size);
 
-  const a = array[inicio];
-  const b = array[meio];
-  const c = array[fim];
-
-  if ((a - b) * (c - a) >= 0) {
-    return a;
-  } else if ((b - a) * (c - b) >= 0) {
-    return b;
-  } else {
-    return c;
-  }
-}
-
-// Função para encontrar o pivô pelo procedimento Acha Pivô
-function achaPivo(array) {
-  for (let i = 1; i < array.length; i++) {
-    if (array[i] < array[i - 1]) {
-      return array[i];
+        if(i != j)
+        {
+            [arr[i],arr[j]] = [arr[j],arr[i]];
+        }
+        else
+        {
+            i--;
+        }
     }
-  }
-  // Se todos os elementos estiverem ordenados, retorna o último elemento
-  return array[array.length - 1];
+
+    return arr;
 }
 
-// Função para salvar um array em um arquivo
-function salvarArrayEmArquivo(nomeArquivo, array) {
-  fs.writeFile(nomeArquivo, JSON.stringify(array), (err) => {
-    if (err) {
-      console.error('Erro ao salvar o arquivo:', err);
-      return;
+function randPartition(arr,l,h)
+{
+    let rand = getRandomArbitrary(l, h);
+    [arr[rand],arr[h]] = [arr[h],arr[rand]];
+    let pivot = arr[rand];
+    let i = l;
+    for (let j = l; j < h; j++) 
+    {
+        if (arr[j] < pivot) 
+        {
+            [arr[i],arr[j]] = [arr[j],arr[i]];
+            i++;
+        }
     }
-    //console.log(`Array salvo em "${nomeArquivo}"`);
-  });
+
+    [arr[i],arr[h]] = [arr[h],arr[i]];
+
+    return i;
 }
 
-function calculaMedia(array) {
-  const soma = array.reduce((acumulador, valorAtual) => acumulador + valorAtual, 0);
-  const media = soma / array.length;
-  return media;
+function kMininum(arr,l,h,k)
+{
+    if(l==h)
+    {
+        return arr[l];
+    }
+    
+    let q = randPartition(arr,l,h);
+
+    if(k == q)
+    {
+        return arr[q];
+    }
+
+    if(k < q)
+    {
+        return kMininum(arr,l, q - 1,k);
+    }
+    else
+    {
+        return kMininum(arr,q + 1,h,k);
+    }
 }
 
-function main() {
-  var tamanhoArray = 100000; // Tamanho do array desejado
-  const grauDesordenamento = 'muito'; // 'pouco', 'medio', 'muito'
+function findPivot(arr,l,h)
+{
+    let prox = l + 1;
+    for(let i = l; i < h; i++)
+    {
+        if(arr[i] > arr[prox])
+        {
+            return arr[i];
+        }
+        else
+        {
+            prox++;
+        }
+    }
 
-  let arrayEmbaralhado;
-  let inicio;
-  let fim;
-  let arrayOrdenado;
-  let tempoExecucao;
-  let tipoPivo;
-  let times = [];
-  let N = 10;
-  // executa quickSort N vezes e salva média
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  for (let i = 0; i < N; i++) {
-    arrayEmbaralhado = criarArrayEmbaralhado(tamanhoArray, grauDesordenamento);       
-    tipoPivo = 'mediana' // 'primeira', 'cental', 'media', 'aleatorio', 'mediana', 'achaPivo'
-    inicio = performance.now(); // inicio timer
-    arrayOrdenado = quicksort([...arrayEmbaralhado], 0, (arrayEmbaralhado.length - 1), tipoPivo); // quicksort(array, inicio, fim, tipoPivo)
-    fim = performance.now(); // fim timer
-    tempoExecucao = fim - inicio;
-    times[i] = tempoExecucao;
-  }
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  let mediaTempoMilisegundos = calculaMedia(times);
-  let mediaTempoSegundos = calculaMedia(times)/1000;
-  console.log("\nMédia de tempo de " + qntRepeticoes + " execuções quickSort com "+ tamanhoArray + " elementos com " +
-   grauDesordenamento + " desordenamento " + "e pivo " + tipoPivo + ": " +
-   "\n", mediaTempoSegundos.toFixed(5) + " segundos");  console.log(mediaTempoMilisegundos.toFixed(5) + " milisegundos");
-  salvarArrayEmArquivo('array_embaralhado.json', arrayEmbaralhado);
-  salvarArrayEmArquivo('array_ordenado.json', arrayOrdenado);
+    return -1;
 }
 
-main();
+function pivotChoice(arr,l,h)
+{
+    let pivot;
+    switch(pivotCase)
+    {
+        case 'firstElem':
+            pivot = arr[l];
+            break;
+        case 'middleElem':
+            pivot = arr[Math.floor((h - l)/2) + l]
+            break;
+        case 'meanFML':
+            pivot = (arr[l] + arr[Math.floor((h - l)/2) + l] + arr[h])/3;
+            break;
+        case 'rand':
+            pivot = arr[getRandomArbitrary(l,h)];
+            break;
+        case 'medianElem':
+            let auxArr = arr.slice(l,h+1);
+            pivot = kMininum(auxArr,0,auxArr.length - 1,Math.floor((auxArr.length + 1)/2) - 1);
+            break;
+        case 'findPivot':
+            pivot = findPivot(arr,l,h);
+            break;
+        default:
+            pivot = -1;
+            break;
+    }
+
+    return pivot;
+}
+
+function partition(arr,l,h)
+{
+    let pivot = pivotChoice(arr,l,h);
+
+    if(pivot < 0) return -1;
+
+    let i = l - 1;
+    let j = h + 1;
+
+    while(true)
+    {
+        do{
+            i++;
+        } while(arr[i] < pivot);
+
+        do{
+            j--;
+        } while(arr[j] > pivot);
+
+        if(i >= j)
+        {
+            return j;
+        }
+
+        [arr[i],arr[j]] = [arr[j],arr[i]];
+    }
+}
+
+function quickSort(arr,l,h)
+{
+    if(l >= 0 && h >= 0 && l < h)
+    {
+        let p = partition(arr,l,h);
+        if(p >= 0)
+        {
+            quickSort(arr,l,p);
+            quickSort(arr,p+1,h);
+        }
+    }
+}
+
+function generateData(maxExp,degree)
+{
+    let size = 10;
+    let labels = [];
+    let meanTimes = [];
+
+    for(let i = 2; i <= maxExp;i++)
+    {
+        let times = [];
+        size = size * 10;
+        labels.push(`10^(${i})`);
+
+        for(let j = 0; j < 10; j++)
+        {
+            let arr = generateUnorderedArrayByDegree(degree,size);
+            let start = performance.now();
+            quickSort(arr,0,arr.length - 1);
+            let end = performance.now();
+            times.push(end - start);
+        }
+
+        meanTimes.push(averageArray(times));
+    }
+
+    return [labels,meanTimes];
+}
+
+function generateHTMLTemplate(id)
+{
+    let temp = `<div class="col-4"> <canvas id="${id}"></canvas> </div>\n`;
+    return temp;
+}
+
+function generateJSTemp(id,labels,times,title)
+{
+    let temp = `new Chart(document.getElementById('${id}'), {
+        type: 'line',
+        data: {
+          labels: ${JSON.stringify(labels)},
+          datasets: [{
+            label: 'Média de Tempo',
+            data: ${JSON.stringify(times)},
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          },
+          plugins: {
+            title: {
+                display: true,
+                text: '${title}'
+            }
+        }
+        }
+      });\n`;
+
+      return temp;
+}
+
+function generateHTMLPage(html,js)
+{
+    let temp = `<!DOCTYPE html>
+    <html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Trabalho APA - Gráficos</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                ${html.join('')}
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        <script>
+            ${js.join('')}
+        </script>
+    </body>
+    </html>`
+
+    fs.writeFile('./graph.html',temp,err =>{
+        if(err)
+        {
+            console.error(err);
+        }
+    });
+}
+
+function runTests()
+{
+    let pivots = ['firstElem','middleElem','meanFML','rand','medianElem','findPivot'];
+    let degrees = [5,25,45];
+    let htmlTemps = [];
+    let jsTemps = [];
+
+    for(let i = 0; i < pivots.length; i++)
+    {
+        pivotCase = pivots[i];
+        for(let j = 0; j < degrees.length; j++)
+        {
+            [labels,times] = generateData(5,degrees[j]);
+            htmlTemps.push(generateHTMLTemplate(`${pivots[i]}${j}`));
+            jsTemps.push(generateJSTemp(`${pivots[i]}${j}`,labels,times,`${pivots[i]} ${degrees[j]}%`));
+        }
+    }
+
+    generateHTMLPage(htmlTemps,jsTemps);
+}
+
+runTests();
